19322ab35b4555c782474d92a01873e52f35ec98
ivy <ivana_lim@dlsu.edu.ph>
2020-04-14 10:22:53 +0000

    initial commit

---
+++ PRcalculator.py
@@ -0,0 +1,63 @@
+import discord
+from discord.ext import commands
+import json
+
+with open("credentials.json") as creds:
+    creds = json.loads(creds.read())
+    TOKEN = creds["TOKEN"]  
+
+tierlist_link = 'https://github.com/mha-smashrising/tierlist'
+github_link = 'https://github.com/mha-smashrising/'
+
+client = discord.Client()
+
+class Bot(commands.Bot):
+    def __init__(self):
+        super(Bot, self).__init__(command_prefix="$", case_insensitive=True)
+        self.pool = None
+
+bot = Bot()
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user)
+    # print(bot.user.id)
+    print('------')
+
+
+# @client.event
+# async def on_ready():
+#     print('We have logged in as {0.user}'.format(client))
+
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
+
+#     if message.content.startswith('$tierlist'):
+#         await message.channel.send('https://github.com/mha-smashrising/tierlist')
+
+# About Command
+@bot.command()
+async def about(ctx):
+    aboutEmbed = discord.Embed(
+        title="A discord.py bot for Smash Rising Discord Server",
+        # description="Type ``%s`` for a list of available commands\n__\n__" % "o!help",
+        color=0xD800FF
+    )
+    aboutEmbed.set_author(name=bot.user.name + ' - About', icon_url=bot.user.avatar_url)
+    aboutEmbed.set_thumbnail(url=bot.user.avatar_url)
+    aboutEmbed.add_field(name="Written By", value=(await bot.application_info()).owner.mention)
+    # aboutEmbed.add_field(name="Version", value=ver, inline=False)
+    aboutEmbed.add_field(name="Git Repository", value=github_link)
+    # aboutEmbed.add_field(name="Contributor(s)", value=f"{bot.get_user(119149398306455552).mention} - Data Collection")
+    await ctx.send(embed=aboutEmbed)
+
+# tierlist
+@bot.command()
+async def tierlist(ctx):
+    # await ctx.send(arg + " <-- this means the bot is online.. nya >.<")
+    await ctx.send(tierlist_link)
+
+bot.run(TOKEN)
